674804396b59306f4f1ac9e5959e9294a4821ba0
kaisa <bnytezz@gmail.com>
2023-03-29 10:41:28 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
-import {create, Workbench} from "@dtinsight/molecule";
+import {create, molecule, Workbench} from "@dtinsight/molecule";
 import '@dtinsight/molecule/esm/style/mo.css';
+import {IExtensionService} from "@dtinsight/molecule/esm/services";
 
 const moInstance = create({
-  extensions: [],
+  extensions: [
+    {
+      id: "InitializeExtension",
+      name: "Initialize Extension",
+      activate(extensionCtx: IExtensionService) {
+        const items = molecule.activityBar.getState().data || [];
+        const {ACTIVITY_BAR_GLOBAL_ACCOUNT} = molecule.builtin.getConstants();
+        const target = items.find((item) => item.id === ACTIVITY_BAR_GLOBAL_ACCOUNT);
+        if (target) {
+          target.contextMenu = [
+            {
+              id: 'login',
+              name: '去登陆',
+              icon: 'log-in',
+              onClick: () => alert("被点击了，看看有没有被收起"),
+            }
+          ]
+        }
+      },
+      dispose(extensionCtx: IExtensionService) {
+
+      },
+    }
+  ],
   defaultLocale: 'zh-CN'
 });
 const MoleculeProvider = () => moInstance.render(<Workbench />);
